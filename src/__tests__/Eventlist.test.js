dea120d252b6b395fed7e39bb4c357278b38e924
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-08-12 18:58:04 +0200

    add mockData

--- src/__tests__/Eventlist.test.js
+++ src/__tests__/Eventlist.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-
+import {mockData} from '../mock-data';
 //components
 import EventList from '../components/eventlist/event-list';
 import Event from '../components/event/event';
@@ -8,9 +8,9 @@ import Event from '../components/event/event';
 describe('<EventList/> component', () => {
 
   test('render correct number of events', () => {
-    const EventListWrapper = shallow(<EventList events={[{id:1},{id:2},{id:3},{id:4}]}  />);
+    const EventListWrapper = shallow(<EventList events={mockData}  />);
     //test will only pass if EventList renders exactly four events from its prop events
-    expect(EventListWrapper.find(Event)).toHaveLength(4);
+    expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
     
   });
 

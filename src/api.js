98db741c5b30ee511e390a2d9d6f3d3dbd728fc4
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-08-19 19:08:20 +0200

    add get access token function

--- src/api.js
+++ src/api.js
@@ -6,13 +6,46 @@
  * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
  * The Set will remove all duplicates from the array.
  */
- import {mockData} from './mock-data';
+import {mockData} from './mock-data';
+import axios from 'axios';
 
- export const getEvents = async () => {
-  return mockData;
+
+const checkToken = async (accessToken) => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then((res) => res.json())
+    .catch((error) => error.json());
+
+  return result;
 };
 
- export const extractLocations = (events) => {
+export const getAccessToken = async () => {
+    const accessToken = localStorage.getItem('access_token');
+    const tokenCheck = accessToken && (await checkToken(accessToken));
+
+    if (!accessToken || tokenCheck.error) {
+      await localStorage.removeItem("access_token");
+      const searchParams = new URLSearchParams(window.location.search);
+      const code = await searchParams.get("code");
+      if (!code) {
+        const results = await axios.get(
+          "https://t8ageqechl.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+        );
+        const { authUrl } = results.data;
+        return (window.location.href = authUrl);
+      }
+      return code && getToken(code);
+    }
+    return accessToken;
+}
+
+
+export const getEvents = async () => {
+ return mockData;
+};
+
+export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
   var locations = [...new Set(extractLocations)];
   return locations;

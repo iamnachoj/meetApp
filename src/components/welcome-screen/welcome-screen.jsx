e4d118b0bd745580a246c7edb0ac6ca01d5d8b3d
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-08-27 16:55:26 +0200

    add right link to privacy policy

--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -10,14 +10,14 @@ function WelcomeScreen(props) {
       <div className="button_cont" align="center">
         <div class="google-btn">
           <div class="google-icon-wrapper">
-            <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log o.svg" alt="Google sign-in" />
+            <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="Google sign-in" />
           </div>
           <button onClick={() => { props.getAccessToken() }} rel="nofollow noopener"class="btn-text">
             <b>Sign in with google</b>
           </button>
         </div>
       </div>
-      <a href="https://iamnachoj.github.io/meet/privacy.html" rel="nofollow noopener" > Privacy policy </a>
+      <a href="https://iamnachoj.github.io/meetApp/privacy.html" rel="nofollow noopener" > Privacy policy </a>
     </div>
   )
   : null

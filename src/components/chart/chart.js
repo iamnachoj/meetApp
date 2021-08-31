b677b3779cae650297e8a845107821b155f9d36c
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-08-31 15:57:13 +0200

    add tooltip labelformatter

--- src/components/chart/chart.js
+++ src/components/chart/chart.js
@@ -28,7 +28,7 @@ export default class Chart extends PureComponent {
           <CartesianGrid />
           <XAxis type="category" dataKey="city" name="city" />
           <YAxis type="number" dataKey="number" name="Number of events" />
-          <Tooltip labelFormatter=""/>
+          <Tooltip labelFormatter="Details"/>
           <Scatter name="A school" data={this.getData()} fill="#8884d8" />
         </ScatterChart>
       </ResponsiveContainer>

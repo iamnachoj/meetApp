e6fdedc6fdbc8cbe3e3e3b79b4e8978f09df9fd3
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-08-31 12:23:44 +0200

    title chart tooltip set to none

--- src/components/chart/chart.js
+++ src/components/chart/chart.js
@@ -28,7 +28,7 @@ export default class Chart extends PureComponent {
           <CartesianGrid />
           <XAxis type="category" dataKey="city" name="city" />
           <YAxis type="number" dataKey="number" name="Number of events" />
-          <Tooltip labelFormatter="City"/>
+          <Tooltip labelFormatter=""/>
           <Scatter name="A school" data={this.getData()} fill="#8884d8" />
         </ScatterChart>
       </ResponsiveContainer>
